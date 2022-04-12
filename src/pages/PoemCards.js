c2164db9bd425a4643b9777a43109df3b277a669
Lisa Trovato-Monastra <l.trovato-monastra@posteo.de>
2022-04-12 14:22:04 +0200

    added poem collection filter

--- src/pages/PoemCards.js
+++ src/pages/PoemCards.js
@@ -8,20 +8,32 @@ import {
   Container,
 } from "@mui/material";
 import { Outlet, useNavigate } from "react-router-dom";
-import { poems } from "../assets/data/poems/poems";
+import { poems as poemData } from "../assets/data/poems/poems";
 import { poemCollections } from "../assets/data/poems/poemCollections";
+import { useState, useEffect } from "react";
 
 export default function Cards() {
+  const [poems, setPoems] = useState([]);
   let navigate = useNavigate();
+
+  useEffect(() => {
+    setPoems(poemData);
+  }, []);
 
   function goToPoem(id) {
     navigate(`/poems/${id}`);
   }
 
+  function filterCollection(collection) {
+    setPoems(poemData.filter((poem) => poem.collection === collection));
+  }
+
   return (
     <Container maxWidth="md">
       <Box>
-        <h1 className="page-title">some poems</h1>
+        <h1 className="page-title" onClick={() => setPoems(poemData)}>
+          some poems
+        </h1>
       </Box>
       <ButtonGroup
         variant="text"
@@ -29,7 +41,11 @@ export default function Cards() {
         aria-label="text small button group"
       >
         {poemCollections.map((item) => (
-          <Button className={item.color} key={item.name}>
+          <Button
+            className={item.color}
+            key={item.name}
+            onClick={() => filterCollection(item.name)}
+          >
             {item.name}
           </Button>
         ))}
