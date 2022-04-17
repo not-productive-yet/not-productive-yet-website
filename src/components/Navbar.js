f4b18e3c64bbf11c1a1110b78d9b01982abb4b6c
Lisa Trovato-Monastra <l.trovato-monastra@posteo.de>
2022-04-17 17:29:30 +0200

    fixed navbar (probably)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,42 @@
-import { Drawer, List, ListItem, useMediaQuery } from "@mui/material";
+import { Drawer, useMediaQuery } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Link } from "react-router-dom";
 import { useState } from "react";
+import NavList from "./NavList";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
-  const isWideScreen = useMediaQuery("(min-width:900px)");
   function openMenu() {
     setOpen(true);
   }
   const toggleDrawer = () => {
     setOpen(!open);
   };
+  const isWideScreen = useMediaQuery("(min-width:800px)");
+
   return (
     <>
-      <MenuIcon
-        className={`menu-icon ${isWideScreen ? "hidden" : ""}`}
-        onClick={openMenu}
-      />
-      <Drawer
-        anchor="left"
-        variant={isWideScreen ? "permanent" : "temporary"}
-        open={open}
-        onClose={toggleDrawer}
-        onClick={toggleDrawer}
-      >
-        <List>
-          <h2 className="nav-title">
-            <Link className="nav-link" to="/">
-              not-prod-yet
-            </Link>
-          </h2>
-          {["Poems", "Stories", "Spare Thoughts", "Other"].map(
-            (text, index) => (
-              <ListItem button key={text}>
-                <Link className="nav-link" to="/poems">
-                  {text}
-                </Link>
-              </ListItem>
-            )
-          )}
-        </List>
-      </Drawer>
+      {isWideScreen ? (
+        <Drawer class="nav navbar-desktop" anchor="left" variant="permanent">
+          <NavList />
+        </Drawer>
+      ) : (
+        <>
+          <MenuIcon
+            className={`menu-icon ${isWideScreen ? "hidden" : ""}`}
+            onClick={openMenu}
+          />
+          <Drawer
+            className="nav navbar-mobile"
+            anchor="left"
+            variant={isWideScreen ? "permanent" : "temporary"}
+            open={open}
+            onClose={toggleDrawer}
+            onClick={toggleDrawer}
+          >
+            <NavList />
+          </Drawer>
+        </>
+      )}
     </>
   );
 }
